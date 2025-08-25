91a4ff4f780df3968bddf725f02328ca2ac457cc
rin-create-keep <aisudaisukinandayo@gmail.com>
2025-08-26 02:55:14 +0900

    Add files via upload

---
+++ leapyear-loop.py
@@ -0,0 +1,9 @@
+import calendar
+
+while True:
+    year = int(input("何年生まれですか？"))
+    if calendar.isleap(year):
+        print(f"{year}年はうるう年です")
+        break
+    else:
+        print(f"{year}年はうるう年ではありません")
