782e9c0dd5562ded3d9d88e43ac43e4be9f772e7
rin-create-keep <aisudaisukinandayo@gmail.com>
2025-11-26 05:09:37 +0900

    Delete leapyear-loop2.py

--- leapyear-loop2.py
+++
@@ -1,9 +0,0 @@
-import calendar
-
-while True:
-    year = int(input("何年生まれですか？"))
-    if calendar.isleap(year):
-        print(year, "年はうるう年です")
-        break
-    else:
-        print(year, "年はうるう年ではありません")
